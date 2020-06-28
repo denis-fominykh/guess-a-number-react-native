5f19b73ce0ea832f43e5c35957d96132730a93eb
denis-fominykh <fominykhdenis@gmail.com>
2020-06-29 02:15:25 +0300

    creating a start game screen component

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 const App: FC = () => {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/StartGameScreen/index.ts
@@ -0,0 +1,3 @@
+import StartGameScreen from './StartGameScreen';
+
+export default StartGameScreen;
