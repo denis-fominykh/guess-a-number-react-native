5930749a5bec236ab492b7f1d097790025c5d72f
denis-fominykh <fominykhdenis@gmail.com>
2020-07-08 14:21:48 +0300

    adding a san francisco font

--- services/fetchFonts.ts
+++ services/fetchFonts.ts
@@ -4,5 +4,7 @@ export const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('../assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+    'san-francisco-display': require('../assets/fonts/SFUIDisplay-Regular.ttf'),
+    'san-francisco-display-bold': require('../assets/fonts/SFUIDisplay-Bold.ttf'),
   });
 };
