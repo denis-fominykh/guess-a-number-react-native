0bafb7f0df65f63b69d16c45e2e1a4d8da593812
denis-fominykh <fominykhdenis@gmail.com>
2020-07-12 14:03:42 +0300

    using dimensions API for the game screen

--- screens/GameScreen/GameScreenStyle.ts
+++ screens/GameScreen/GameScreenStyle.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,13 +9,13 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%',
   },
   listContainer: {
     flex: 1,
-    width: '80%',
+    width: Dimensions.get('window').width > 350 ? '60%' : '80%',
   },
   list: {
     alignItems: 'center',
